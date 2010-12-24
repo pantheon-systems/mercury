987f48401f5feab07997736393152d1f26aed3a8
Greg Coit <greg@getpantheon.com>
2010-12-24 09:47:48 -0800

    replaced direct xml file creation with postback/junit hook

--- fab/monitoring.py
+++ fab/monitoring.py
@@ -12,7 +12,7 @@ from xml.dom.minidom import Document
 def check_load_average(limit):
     loads = os.getloadavg()
     if (float(loads[0]) > float(limit)):
-        _error('Load average is %s which is above the threshold of %s.' % (str(loads[0]), str(limit)))
+        _warning('Load average is %s which is above the threshold of %s.' % (str(loads[0]), str(limit)))
     else:
         _success('Load average is %s which is below the threshold of %s.' % (str(loads[0]), str(limit)))
 
@@ -21,7 +21,7 @@ def check_disk_space(filesystem, limit):
     s = os.statvfs(filesystem)
     usage = (s.f_blocks - s.f_bavail)/float(s.f_blocks) * 100
     if (float(usage) > float(limit)):
-        _error('Disk usage of %s is at %s percent which is above the threshold of %s percent.' % (filesystem, str(usage), str(limit)))
+        _warning('Disk usage of %s is at %s percent which is above the threshold of %s percent.' % (filesystem, str(usage), str(limit)))
     else:
         _success('Disk usage of %s is at %s percent which is above the threshold of %s percent.' % (filesystem, str(usage), str(limit)))
 
@@ -31,7 +31,7 @@ def check_swap_usage(limit):
     swap_used = local("free | grep -i swap | awk '{print $3}'")
     usage = float(swap_used)/float(swap_total) * 100
     if (usage > float(limit)):
-        _error('Swap usage is a %s percent which is above the threshold of %s percent.' % (str(usage), str(limit)))
+        _warning('Swap usage is a %s percent which is above the threshold of %s percent.' % (str(usage), str(limit)))
     else:
         _success('Swap usage is a %s percent which is below the threshold of %s percent.' % (str(usage), str(limit)))
 
@@ -39,7 +39,7 @@ def check_swap_usage(limit):
 def check_io_wait_time(limit):
     iowait = local("vmstat | grep -v [a-z] | awk '{print $16}'").rstrip()
     if (float(iowait) > float(limit)):
-        _error('IO wait times are at %s percent which is above the threshold of %s percent.' % (str(iowait), str(limit)))
+        _warning('IO wait times are at %s percent which is above the threshold of %s percent.' % (str(iowait), str(limit)))
     else:
         _success('IO wait times are at %s percent which is below the threshold of %s percent.' % (str(iowait), str(limit)))
 
@@ -49,7 +49,7 @@ def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
         messages = list()
         report = local('mysqlreport')
         if report.failed:
-            _fail('mysql server does not appear to be running: %s' % report)
+            _failure('mysql server does not appear to be running: %s' % report)
         for line in report.splitlines():
             #check for slow wait times:
             if ('Slow' in line and 'Log' in line):
@@ -81,7 +81,7 @@ def check_mysql(slow_query_limit, memory_usage, innodb_memory_usage, threads):
                
         message = ' '.join(messages)
         if 'above' in message: 
-            _error(message)
+            _warning(message)
         else:
             _success(message)
 
@@ -91,7 +91,7 @@ def check_ldap():
         local('ldapsearch -H ldap://auth.getpantheon.com -x -ZZ')
         _success('ldap responded')
     except:
-        _fail('Cannot connect to LDAP on localhost.')
+        _failure('Cannot connect to LDAP on localhost.')
 
 
 def check_apache(url):
@@ -114,7 +114,7 @@ def check_pound_via_socket(port):
         s.shutdown(2)
         _success('pound responded')
     except:
-        _fail('Cannot connect to Pound on %s %s.' % ('localhost', str(port)))
+        _failure('Cannot connect to Pound on %s %s.' % ('localhost', str(port)))
 
 
 def check_memcached(port):
@@ -125,14 +125,14 @@ def check_memcached(port):
         s.shutdown(2)
         _success('memcached responded')
     except:
-        _fail('Cannot connect to Memcached on %s %s.' % ('localhost', str(port)))
+        _failure('Cannot connect to Memcached on %s %s.' % ('localhost', str(port)))
 
 
 def _test_url(service, url):
     connection = urllib.urlopen(url)
     status = connection.getcode()
     if (status >=  400):
-        _error('%s returned an error code of %s.' % (service, status))
+        _warning('%s returned an error code of %s.' % (service, status))
     else:
         _success('%s returned an error code of %s.' % (service, status))
 # TODO: figure out what to search for from the following output
@@ -141,108 +141,18 @@ def _test_url(service, url):
 
 
 def _success(message):
-    f = open(os.environ.get('WORKSPACE') + '/results.xml', 'w')
-
-    doc = Document()
-
-    testsuites = doc.createElement("testsuites")
-    doc.appendChild(testsuites)
-
-    testsuite = doc.createElement("testsuite")
-    testsuite.setAttribute("name", "MyTest")
-    testsuite.setAttribute("file", ".")
-    testsuite.setAttribute("tests", "1")
-    testsuite.setAttribute("assertions", "0")
-    testsuite.setAttribute("failures", "0")
-    testsuite.setAttribute("errors", "0")
-    testsuite.setAttribute("time", "1")
-    testsuites.appendChild(testsuite)   
-
-    testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "error")
-    testcase.setAttribute("assertions", "0")
-    testcase.setAttribute("time", "1")
-    testsuite.appendChild(testcase)
-
-    etext = doc.createTextNode(message)
-    testcase.appendChild(etext)
-
-    f.write(doc.toprettyxml(indent="  "))
-    f.close()
-
-    #now print so we see it in hudson console output
-    print(message)
+    response = dict()
+    response['status'] = 'SUCCESS'
+    response['msg'] = 'message'
 
 
-def _error(message):
-    f = open(os.environ.get('WORKSPACE') + '/results.xml', 'w')
-
-    doc = Document()
-
-    testsuites = doc.createElement("testsuites")
-    doc.appendChild(testsuites)
-
-    testsuite = doc.createElement("testsuite")
-    testsuite.setAttribute("name", "MyTest")
-    testsuite.setAttribute("file", ".")
-    testsuite.setAttribute("tests", "1")
-    testsuite.setAttribute("assertions", "0")
-    testsuite.setAttribute("failures", "0")
-    testsuite.setAttribute("errors", "1")
-    testsuite.setAttribute("time", "1")
-    testsuites.appendChild(testsuite)   
-
-    testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "error")
-    testcase.setAttribute("assertions", "0")
-    testcase.setAttribute("time", "1")
-    testsuite.appendChild(testcase)
-
-    error = doc.createElement("error")
-    testcase.appendChild(error)
-
-    etext = doc.createTextNode(message)
-    error.appendChild(etext)
-
-    f.write(doc.toprettyxml(indent="  "))
-    f.close()
-
-    #now print so we see it in hudson console output
-    print(message)
+def _warning(message):
+    response = dict()
+    response['status'] = 'WARNING'
+    response['msg'] = 'message'
 
 
-def _fail(message):
-    f = open(os.environ.get('WORKSPACE') + '/results.xml', 'w')
-
-    doc = Document()
-
-    testsuites = doc.createElement("testsuites")
-    doc.appendChild(testsuites)
-
-    testsuite = doc.createElement("testsuite")
-    testsuite.setAttribute("name", "MyTest")
-    testsuite.setAttribute("file", ".")
-    testsuite.setAttribute("tests", "1")
-    testsuite.setAttribute("assertions", "0")
-    testsuite.setAttribute("failures", "1")
-    testsuite.setAttribute("errors", "0")
-    testsuite.setAttribute("time", "1")
-    testsuites.appendChild(testsuite)   
-
-    testcase = doc.createElement("testcase")
-    testcase.setAttribute("name", "error")
-    testcase.setAttribute("assertions", "0")
-    testcase.setAttribute("time", "1")
-    testsuite.appendChild(testcase)
-
-    failure = doc.createElement("failure")
-    testcase.appendChild(failure)
-
-    etext = doc.createTextNode(message)
-    failure.appendChild(etext)
-
-    f.write(doc.toprettyxml(indent="  "))
-    f.close()
-
-    #now print so we see it in hudson console output
-    print(message)
+def _failure(message):
+    response = dict()
+    response['status'] = 'FAILURE'
+    response['msg'] = 'message'
